22dafd3f171872ab570f07215d2adb915e5faf07
lee-orr <leeorr@gmail.com>
2022-12-27 18:46:39 +0000

    add bevy examples

---
+++ examples/bevy_asset.rs
@@ -0,0 +1,99 @@
+use bevy::prelude::*;
+use bevy_generative_grammars::{
+    generator::*,
+    tracery::{tracery_asset::TraceryAssetPlugin, StatefulStringGenerator, TraceryGrammar},
+};
+use rand::*;
+
+fn terminal_runner(mut app: App) {
+    println!("Press Enter to Progress, or type 'exit' to exit");
+    for line in std::io::stdin().lines() {
+        let typed: String = line.unwrap_or_default();
+        if typed == "exit" {
+            return;
+        }
+        app.update();
+    }
+}
+
+fn main() {
+    App::new()
+        .set_runner(terminal_runner)
+        .add_plugin(CorePlugin::default())
+        .add_plugin(AssetPlugin::default())
+        .add_plugin(TraceryAssetPlugin::new().with_json(&["json"]))
+        .add_startup_system(setup)
+        .add_system(loaded_grammar)
+        .add_system(progress_story)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let grammar = GrammarHandle(asset_server.load("story.json"), false);
+    commands.insert_resource(grammar);
+}
+
+fn loaded_grammar(
+    mut commands: Commands,
+    mut handle: ResMut<GrammarHandle>,
+    grammars: ResMut<Assets<TraceryGrammar>>,
+) {
+    if handle.1 {
+        return;
+    }
+
+    if let Some(grammar) = grammars.get(&handle.0) {
+        handle.1 = true;
+        println!("Starting New Story!");
+        commands.spawn((
+            StatefulStringGenerator::clone_grammar(grammar),
+            NextPrompt("origin".to_string()),
+        ));
+    } else {
+        println!("Loading Grammar File...");
+    }
+}
+
+#[derive(Component)]
+struct NextPrompt(String);
+
+#[derive(Resource)]
+struct GrammarHandle(Handle<TraceryGrammar>, bool);
+
+fn progress_story(
+    mut commands: Commands,
+    mut handle: ResMut<GrammarHandle>,
+    mut query: Query<(Entity, &mut StatefulStringGenerator, &mut NextPrompt)>,
+) {
+    let mut rng = thread_rng();
+    let mut rng_func = |len| {
+        if len == 0 {
+            0
+        } else {
+            rng.gen_range(0..len)
+        }
+    };
+
+    for (entity, mut generator, mut next_prompt) in query.iter_mut() {
+        if let Some(generated) = generator.generate_at(&next_prompt.0, &mut rng_func) {
+            let mut split = generated.split('|');
+            if let Some(generated) = split.next() {
+                println!("{generated}");
+            }
+            if let Some(next_item) = split.next() {
+                if next_item != "done" {
+                    next_prompt.0 = next_item.to_string();
+                } else {
+                    commands.entity(entity).despawn();
+
+                    println!("Story Complete...");
+                    handle.1 = false;
+                }
+            }
+        } else {
+            eprintln!("failed to generate...");
+        }
+    }
+
+    println!("Continue...")
+}
---
+++ examples/bevy_simple.rs
@@ -0,0 +1,109 @@
+use bevy::prelude::*;
+use bevy_generative_grammars::{
+    generator::*,
+    tracery::{StatefulStringGenerator, TraceryGrammar},
+};
+use rand::*;
+
+const RULES: &[(&str, &[&str])] =  &[
+    (
+        "origin",
+        &["[hero:#creature#][obstacle:#noun#]#intro# there was a #hero# that #encountered# #article# #obstacle#.|next"],
+    ),
+    (   "next",
+        &[
+            "Then, the #hero# decided to #action# #definite# #obstacle#.|finally",
+            "Our adventerous #hero# was ready to #action# #definite# #obstacle#.|finally"
+        ]
+    ),
+    (   "finally",
+        &[
+            "And so, despite #finale# - our #hero# made it back home.|done",
+            "And so - after #finale# - the lonely #hero# had proven their worth.|done"
+        ]
+    ),
+    (
+        "intro",
+        &[
+            "once upon a time",
+            "many years ago",
+            "a long time ago",
+            "in a far away land",
+        ],
+    ),
+    ("creature", &["ant", "rabbit", "giraffe", "lion"]),
+    ("encountered", &["ran into", "found", "saw"]),
+    ("move", &["cross[finale|#went#]","circle[finale|#went#]","explore[finale|#went#]"]),
+    ("fight", &["avoid[finale|#went#]", "fight[finale|#fought#]", "challange[finale|#fought#]"]),
+    ("went", &["a long long journey", "a challanging path"]),
+    ("fought", &["a challanging encounter", "a very close call"]),
+    ("noun", &["[article:a][definite:the][action|#move#]river", "[article:a][definite:the][action|#move#]mountain", "[article:some][definite:them][action|#fight#]monsters"]),
+];
+
+fn terminal_runner(mut app: App) {
+    println!("Press Enter to Progress, or type 'exit' to exit");
+    for line in std::io::stdin().lines() {
+        let typed: String = line.unwrap_or_default();
+        if typed == "exit" {
+            return;
+        }
+        app.update();
+    }
+}
+
+fn main() {
+    let grammar = TraceryGrammar::new(RULES, None);
+
+    App::new()
+        .set_runner(terminal_runner)
+        .insert_resource(grammar)
+        .add_startup_system(setup)
+        .add_system(progress_story)
+        .run();
+}
+
+fn setup(mut commands: Commands, grammar: Res<TraceryGrammar>) {
+    commands.spawn((
+        StatefulStringGenerator::clone_grammar(&grammar),
+        NextPrompt("origin".to_string()),
+    ));
+}
+
+#[derive(Component)]
+struct NextPrompt(String);
+
+fn progress_story(
+    mut commands: Commands,
+    mut query: Query<(Entity, &mut StatefulStringGenerator, &mut NextPrompt)>,
+) {
+    let mut rng = thread_rng();
+    let mut rng_func = |len| {
+        if len == 0 {
+            0
+        } else {
+            rng.gen_range(0..len)
+        }
+    };
+
+    for (entity, mut generator, mut next_prompt) in query.iter_mut() {
+        if let Some(generated) = generator.generate_at(&next_prompt.0, &mut rng_func) {
+            let mut split = generated.split('|');
+            if let Some(generated) = split.next() {
+                println!("{generated}");
+            }
+            if let Some(next_item) = split.next() {
+                if next_item != "done" {
+                    next_prompt.0 = next_item.to_string();
+                } else {
+                    commands.entity(entity).despawn();
+
+                    println!("Story Complete...");
+                }
+            }
+        } else {
+            eprintln!("failed to generate...");
+        }
+    }
+
+    println!("Continue...")
+}
